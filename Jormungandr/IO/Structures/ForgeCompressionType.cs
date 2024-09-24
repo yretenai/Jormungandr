16ff5a7397d03818f8920e5d0ab85e00b8b7787d
Ada <yretenai@gmail.com>
2024-09-24 12:41:27 +0100

    Chore: add comments

--- Jormungandr/IO/Structures/ForgeCompressionType.cs
+++ Jormungandr/IO/Structures/ForgeCompressionType.cs
@@ -1,5 +1,6 @@
 namespace Jormungandr.IO.Structures;
 
+// compression types for version 29.
 public enum ForgeCompressionType : byte {
 	Lzo1x,
 	Lzo1xOpt,
@@ -15,4 +16,8 @@ public enum ForgeCompressionType : byte {
 	None,
 	Zlib,
 	Zstd,
+
+	// Zstd is added in version 30, replaces Lz4.
+	// in Version 28, Zstd is is Oodle
+	// in Version 27 and prior, anything past LZ4HC doesn't exist
 }
